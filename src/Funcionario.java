92c76125d7b453b70848cc59fe2f912cef7b7edd
GuiRuchell <ruchellguilherme100@gmail.com>
2024-03-01 22:51:06 -0300

    implementando to string

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -45,4 +45,15 @@ public class Funcionario {
     public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
     }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", cpf=" + cpf +
+                ", cargo='" + cargo + '\'' +
+                ", salario=" + salario +
+                ", endereco=" + endereco +
+                '}';
+    }
 }
